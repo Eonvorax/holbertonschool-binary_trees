cf89f1e0946e91142879178ce063909192dd532c
Eonvorax <130477483+Eonvorax@users.noreply.github.com>
2023-09-11 09:01:39 +0000

    Task 0: New node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - creates a binary tree node
+ * @parent: pointer to parent of node to create
+ * @value: value to put in the node
+ *
+ * Return: pointer to created node
+*/
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node = NULL;
+
+	node = malloc(sizeof(binary_tree_t) * 1);
+	if (node == NULL)
+	{
+		return (NULL);
+	}
+	node->n = value;
+	node->parent = parent;
+	node->left = NULL;
+	node->right = NULL;
+	return (node);
+}
