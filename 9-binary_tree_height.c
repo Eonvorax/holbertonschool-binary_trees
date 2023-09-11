1d53dd0a5c2b264103d42bc74317b7de5060d6eb
Eonvorax <130477483+Eonvorax@users.noreply.github.com>
2023-09-11 14:34:04 +0000

    Task 09: Height

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure
+ *
+ * Return: nothing
+*/
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (0);
+	}
+	return (MAX(binary_tree_height(tree->left), binary_tree_height(tree->right))
+	 + 1);
+}
