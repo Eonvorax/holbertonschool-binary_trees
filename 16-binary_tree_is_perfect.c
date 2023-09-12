a95e4fc0a414b8467f47b90a1a3cddaa93bb8a26
Eonvorax <130477483+Eonvorax@users.noreply.github.com>
2023-09-12 12:12:34 +0000

    Fix: new strategy using fewer functions

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -12,8 +12,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		return (0);
 	}
-	return (MAX(binary_tree_height(tree->left), binary_tree_height(tree->right))
-	 + 1);
+	return (MAX(binary_tree_height(tree->left), binary_tree_height(tree->right)) + 1);
 }
 
 /**
@@ -55,12 +54,19 @@ int binary_tree_is_full(const binary_tree_t *tree)
  * @tree: pointer to the root node of the tree to check
  *
  * Return: 1 if perfect, 0 if not
-*/
+ */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-	{
 		return (0);
-	}
-	return (binary_tree_balance(tree) == 0 && binary_tree_is_full(tree) == 1);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	if (binary_tree_height(tree->left) != binary_tree_height(tree->right))
+		return (0);
+
+	return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
+
+	/*return (binary_tree_balance(tree) == 0 && binary_tree_is_full(tree) == 1);*/
 }
