8ff5ed5aa93401597e33c70666d499a016543b14
Eonvorax <130477483+Eonvorax@users.noreply.github.com>
2023-09-12 12:16:20 +0000

    Fix: betty style

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -12,41 +12,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		return (0);
 	}
-	return (MAX(binary_tree_height(tree->left), binary_tree_height(tree->right)) + 1);
-}
-
-/**
- * binary_tree_balance - measures the balance factor of a binary tree
- * @tree: pointer to the root node of the tree to measure the balance factor
- *
- * Return: height of left branch minus height of right branch
- */
-int binary_tree_balance(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-	{
-		return (0);
-	}
-	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
-}
-
-/**
- * binary_tree_is_full - checks if a binary tree is full
- * @tree: pointer to the root node of the tree to check
- *
- * Return: 1 if full, 0 if not
- */
-int binary_tree_is_full(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-	{
-		return (0);
-	}
-	if ((tree->left == NULL) && (tree->right == NULL))
-	{
-		return (1);
-	}
-	return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	return (MAX(binary_tree_height(tree->left),
+	 binary_tree_height(tree->right)) + 1);
 }
 
 /**
@@ -66,7 +33,6 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (binary_tree_height(tree->left) != binary_tree_height(tree->right))
 		return (0);
 
-	return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
-
-	/*return (binary_tree_balance(tree) == 0 && binary_tree_is_full(tree) == 1);*/
+	return (binary_tree_is_perfect(tree->left) &&
+	 binary_tree_is_perfect(tree->right));
 }
