500ca4fe60539eeb7208cab914bc054352a0a49e
Eonvorax <130477483+Eonvorax@users.noreply.github.com>
2023-09-12 11:58:41 +0000

    Fix: handling NULL tree case

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -58,5 +58,9 @@ int binary_tree_is_full(const binary_tree_t *tree)
 */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	if (tree == NULL)
+	{
+		return (0);
+	}
 	return (binary_tree_balance(tree) == 0 && binary_tree_is_full(tree) == 1);
 }
